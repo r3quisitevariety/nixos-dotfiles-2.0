cd95f7b47f0aaa7b25b39c54d8c1ca113f7925b2
rqvrty <Onoruuu@gmail.com>
2026-04-04 18:26:32 -0400

    unstable foot for compatibility with stable setup

--- hosts/nitro5/configuration.nix
+++ hosts/nitro5/configuration.nix
@@ -323,7 +323,8 @@ in
 
     # ── Wayland / Compositor ──────────────────────────────────────────────────
     inputs.noctalia.packages.${pkgs.stdenv.hostPlatform.system}.default
-    fuzzel foot #ghostty
+    fuzzel 
+    nixpkgs-unstable.foot #ghostty
     wlr-randr xwayland-satellite   # xwayland for niri
     hyprshot #cliphist              # screenshot; clipboard (niri)
     kdePackages.qt6ct pywalfox-native # for theming
