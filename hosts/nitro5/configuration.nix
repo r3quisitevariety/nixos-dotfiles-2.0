d63ca4126005f399378e905249254942c89c2f82
rqvrty <Onoruuu@gmail.com>
2026-04-18 19:27:21 -0400

    fix steam issues

--- hosts/nitro5/configuration.nix
+++ hosts/nitro5/configuration.nix
@@ -265,14 +265,14 @@
   programs.chromium.enable = true;
   programs.steam = {
     enable = true;
-    package = pkgs.steam.override {
-      extraProfile = ''
-        # Allows Monado/WiVRn to be used
-        export PRESSURE_VESSEL_IMPORT_OPENXR_1_RUNTIMES=1
-        # Fixes timezones on VRChat
-        unset TZ
-      '';
-    };
+    #    package = pkgs.steam.override {
+    #      extraProfile = ''
+    #        # Allows Monado/WiVRn to be used
+    #        export PRESSURE_VESSEL_IMPORT_OPENXR_1_RUNTIMES=1
+    #        # Fixes timezones on VRChat
+    #        unset TZ
+    #      '';
+    #    };
   };
   programs.nix-ld.enable = true; # FHS compat fixes
   programs.pay-respects.enable = true; # press F to pay respects
