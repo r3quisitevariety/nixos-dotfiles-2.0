ad1cf490bcc3b89290bb91ec2a52a9f43c760f0b
rqvrty <Onoruuu@gmail.com>
2026-05-10 21:33:48 -0400

    fix some packaging discrepancies

--- home/neovim.nix
+++ home/neovim.nix
@@ -5,11 +5,11 @@
   # i currently have a normie neovim config in ~/.config/nvim and this module is here to manage the deps, though i do plan to make a nix wrapper soon for my entire neovim config
   environment.systemPackages = with pkgs; [
     neovim
-    #lazygit
+    lazygit
     #fzf
     #ripgrep
     #fd
-    #rustup
+    rustup
     #rust-analyzer
   ];
 
