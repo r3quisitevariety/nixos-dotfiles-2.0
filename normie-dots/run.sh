b01b68e5a80e77141605d549936a3e5f66ce8eaf
rqvrty <Onoruuu@gmail.com>
2026-06-07 00:34:27 -0500

    remove dotfiles i dont need

--- normie-dots/run.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-export LD_LIBRARY_PATH="$HOME/bitwig/opt/bitwig-studio/bin:$HOME/bitwig/opt/bitwig-studio/lib/bitwig-studio"
-exec steam-run "$HOME/bitwig/opt/bitwig-studio/bitwig-studio" "$@"
