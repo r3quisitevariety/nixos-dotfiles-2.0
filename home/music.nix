1e739f53c5df9acd5d35d7e9b688ef0104a6301c
rqvrty <Onoruuu@gmail.com>
2026-06-07 00:25:55 -0500

    add rmpc

---
+++ home/music.nix
@@ -0,0 +1,19 @@
+{pkgs, ...}: {
+  programs.rmpc.enable = true;
+
+  services.mpd = {
+    enable = true;
+    musicDirectory = "/home/requisite/Music/h2";
+
+    extraConfig = ''
+      audio_output {
+        type    "pipewire"
+        name    "MPD PipeWire"
+        mixer_type "software"
+      }
+    '';
+  };
+
+  services.mpd-mpris.enable = true;
+  home.packages = [pkgs.playerctl];
+}
