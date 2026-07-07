65858752ee220d3874c76ef4784f5cc223067cff
rqvrty <Onoruuu@gmail.com>
2026-07-07 16:00:54 -0400

    add nix helper as module

---
+++ modules/nh.nix
@@ -0,0 +1,13 @@
+{
+  config,
+  pkgs,
+  ...
+  # nh package is in configuration.nix
+}: {
+  programs.nh = {
+    enable = true;
+    #clean.enable = true;
+    #clean.extraArgs = "--keep-since 4d --keep 3";
+    flake = "/home/requisite/nixos-dotfiles"; # sets NH_OS_FLAKE variable for you
+  };
+}
