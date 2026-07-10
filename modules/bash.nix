b0e354f60310f607d1da5b80bee77c16b90247dd
r3quisitevariety <Onoruuu@gmail.com>
2026-07-10 16:02:42 -0400

    update bash.nix

--- modules/bash.nix
+++ modules/bash.nix
@@ -1,13 +1,16 @@
 {...}: {
+  # make sure .bashrc and bash_profile are removed locally, otherwise home manager will give you an error as it does not want to delete the files.
   programs.bash = {
     enable = true;
     shellAliases = {
       neofetch = "fastfetch";
       v = "nvim";
       g = "git";
+      ls = "ls --color=auto";
+      grep = "grep --color=auto";
     };
     bashrcExtra = "
-      PS1='\[\e[1;32m\]\u@\h\[\e[0m\]:\[\e[1;34m\]\w\[\e[0m\]\$ '
+      PS1='\\[\\e[1;32m\\]\\u@\\h\\[\\e[0m\\]:\\[\\e[1;34m\\]\\w\\[\\e[0m\\]\\$ '
     ";
   };
 }
